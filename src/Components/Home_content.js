9261b5bf1999e7fe6a4a648f2340b8bbbce85d28
Abhinav Tripathi <abhitrip2002@gmail.com>
2023-03-11 14:52:35 +0530

    '💀'

--- src/Components/Home_content.js
+++ src/Components/Home_content.js
@@ -3,16 +3,16 @@ import video from '../assests/video/v1.mp4'
 
 const Home_content = () => {
   return (
-    <div className='flex justify-centre h-screen items-center absolute top-0 left-0'>
-        <div style={{color:'white'}} className="wrapper w-1/2">
-        Find your perfect getaway: Search our extensive database of :
+    <div className='flex justify-center h-screen w-screen items-center absolute top-0 left-0'>
+        <div style={{color:'white'}} className="wrapper w-1/2 m-auto">
+            Find your perfect getaway: Search our extensive database of :
           <span  class="typing-demo">
             Flights, Hotels, and Activities.
           </span>
         </div>
 
         <div className='w-1/2'>
-            {/* <video src={video} autoplay loop className='bg-transparent'></video> */}
+            <video src={video} autoplay loop className='bg-transparent rounded-full m-auto'></video>
         </div>
 
     </div>
