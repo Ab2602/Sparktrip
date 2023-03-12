9c4f2c773a0a14c8dff6c68ffdeeb8f8a0523152
Abhinav Tripathi <76097113+Ab2602@users.noreply.github.com>
2023-03-12 12:45:07 +0530

    Update Home_content.js

--- src/Components/Home_content.js
+++ src/Components/Home_content.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import video from '../assests/video/v1.mp4'
-import { useTypewriter,Cursor } from 'react-simple-typewriter'
-
+// import video from '../assests/video/c2.gif'
+import Typewriterco from './Typewriterco'
 const Home_content = () => {
-  const{text}= useTypewriter=({
-    words: ['FLights','Hotels','And','Activities'],
-    loop: {},
-  });
-  
   return (
-    <div className='flex justify-center h-screen w-screen items-center absolute top-0 left-0'>
-     <div style={{marginRight:'65%',marginBottom:'20%' ,color:'white'}} > Find your perfect getaway: Search our extensive database of: </div>
-    <span>{text}</span>
-    <Cursor/>
-   
+    <div className='flex justify-center absolute h-screen w-screen items-center top-0 left-0'>
+        <div style={{color:'white'}} className=" w-1/2 m-5 text-5xl leading-relaxed ">
+            <span style={{ display: 'inline' }}>Find your <br /> perfect getaway: <br />
+              Search our extensive  <br /> database of : </span>
+            <Typewriterco  style ={{display: 'inline'}}/>
+        </div>
 
+        <div className='w-1/2'>
+            {/* <video src={video} autoplay loop muted className='bg-transparent rounded-full m-auto'></video> */}
+        </div>
     </div>
-    
-
     
   )
 }
