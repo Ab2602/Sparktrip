5120f81534e030d3a23e2e5cc6de75b33ff17df0
Abhinav Tripathi <abhitrip2002@gmail.com>
2023-03-12 13:26:57 +0530

    '💀'

---
+++ src/Components/Typewriterco.js
@@ -0,0 +1,20 @@
+import Typewriter from 'typewriter-effect';
+import React from 'react'
+import '../index.css'
+
+const Typewriterco = () => {
+  return (
+    <div style={{ display: 'inline' }}>
+      <Typewriter
+      options={{
+        strings: ['Flights...', 'Hotels...', 'Activities...'],
+        autoStart: true,
+        loop: true,
+      }}
+     style={{display: 'inline'}}/>
+    </div>
+    
+  )
+}
+
+export default Typewriterco
