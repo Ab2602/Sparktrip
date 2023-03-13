50e2e0a9cacae69217602a69c3c1428a943464a9
Devansh Sharma <devansh.sharma232002@gmail.com>
2023-03-13 22:09:18 +0530

    done with models and buttons

--- src/Components/Home_content.js
+++ src/Components/Home_content.js
@@ -1,23 +1,33 @@
 import React from 'react'
 // import video from '../assests/video/c2.gif'
 import Typewriterco from './Typewriterco'
+
 import '../index.css'
 const Home_content = () => {
+    
+
+
   return (
+    
+      
     <div className='flex justify-center absolute h-screen w-screen items-center top-0 left-0'>
-        <div style={{color:'#ffffffc4'}} className=" w-1/2 m-5 text-5xl leading-relaxed ">
-            <span style={{ display: 'inline',color:'#ffffffc4'}}>Find your <br /> perfect getaway: <br />
+     
+        <div style={{color:'#ffffffc4'}} className="  w-1/2 m-5 text-5xl leading-relaxed ">
+       
+            <span className='text-type' style={{ display: 'inline',color:'#ffffffc4',cursor:'context-menu'}}>Find your <br /> perfect getaway: <br />
               Search our extensive  <br /> database of : </span>
             <Typewriterco  style ={{display: 'inline'}}/>
             <br />
-            <button className='btn bg-black text-white learn'>Learn More</button>
-            <button className='btn bg-white text-black'>Popular Places</button>
+            <button className='lmfb'>Learn More</button>
+            <button className='lmfb'>Find Best Stays</button>
         </div>
 
         <div className='w-1/2'>
             {/* <video src={video} autoplay loop muted className='bg-transparent rounded-full m-auto'></video> */}
         </div>
+     
     </div>
+    
     
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 // import logo from '../assests/logo.png'
 import image from '../assests/image.png'
+import Anywhere from './Anywhere'
+import Anyweek from './Anyweek'
+import AddGuest from './AddGuest'
 
 const Navbar = () => {
+const [openAnywhere, setOpenAnywhere] = useState(false);
+const[openAnyweek,setOpenAnyweek]=useState(false);
+const[openAddGuest,setOpenAddGuest]=useState(false);
+
+
+
   return (
     <div className='flex h-20 items-center justify-between fixed top-0 inset-x-0 z-50 backdrop-blur-lg backdrop-filter bg-opacity-75'>
         {/* Left */}
@@ -16,9 +25,10 @@ const Navbar = () => {
         {/* Mid */}
         <div className='hidden md:flex shadow-sm shadow-gray-400 border rounded-full justify-center items-center relative ml-24'>
             <div className='flex justify-between w-96 absolute text-white mr-11'>
-            <div className='links'> <button className='font-semibold  '>Anywhere</button> </div>
-            <div className='links'>  <button className='font-semibold  '>Any week</button></div>
-            <div className='links'  >  <button className='font-semibold  '>Add guests</button></div>
+            <div className='links'> <button onMouseEnter={() =>setOpenAnywhere(true)} onMouseLeave={()=> setOpenAnywhere(false)} className='font-semibold  '>Anywhere</button> </div>
+
+            <div className='links'>  <button onMouseEnter={()=>setOpenAnyweek(true)} onMouseLeave={()=>setOpenAnyweek(false)} className='font-semibold  '>Any week</button></div>
+            <div className='links'  >  <button onMouseEnter={()=>setOpenAddGuest(true)} onMouseLeave={()=>setOpenAddGuest(false)} className='font-semibold  '>Add guests</button></div>
             </div>
         </div>
         {/* Right */}
@@ -32,8 +42,10 @@ const Navbar = () => {
 	        Contact</a>
             </div>
         </div>
+       {openAnywhere && <Anywhere  />}
+       {openAnyweek && <Anyweek/>}
+      {openAddGuest && < AddGuest/>} 
     </div>
   )
 }
-
 export default Navbar
