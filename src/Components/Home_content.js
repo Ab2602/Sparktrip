5475bd232eda0998df7bb42ce95a9f9a508ad000
Abhinav Tripathi <abhitrip2002@gmail.com>
2023-03-16 14:20:33 +0530

    '💀'

--- src/Components/Home_content.js
+++ src/Components/Home_content.js
@@ -10,10 +10,10 @@ const Home_content = () => {
       
     <div className='flex justify-center absolute h-screen w-screen items-center top-0 left-0'>
      
-        <div style={{color:'#ffffffc4'}} className="  w-1/2 m-5 text-5xl leading-relaxed ">
+        <div style={{color:'#ffffffc4'}} className="  w-1/2 m-5 text-4xl leading-relaxed ">
        
             <span className='text-type ' style={{ display: 'inline',color:'#ffffffc4',cursor:'context-menu'}}>Find your <br /> perfect getaway: <br />
-              Search our extensive  <br /> database of : </span>
+              Search our extensive  <br /> database of </span>
             <Typewriterco  style ={{display: 'inline'}}/>
             <br />
             <button className='lmfb'>Learn More</button>
