4986ae4776d93e1776e5e4ef3e9d76425c87612f
Devansh Sharma <devansh.sharma232002@gmail.com>
2023-07-16 16:18:05 +0530

    done with data fetching

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,64 +1,118 @@
-import * as React from 'react';
-import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {sliderItems} from './CorouselApi';
-import Navbar from './Navbar';
-import Home_content from './Home_content';
-import Searchbar from './Searchbar';
-import Cards from '../HomeCard/Cards.js';
-import Footer from '../Footer/Footer';
-import {list} from '../HomeCard/Card-List';
-
+import * as React from "react";
+import { useState } from "react";
+import axios from "axios";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { sliderItems } from "./CorouselApi";
+import Navbar from "./Navbar";
+import Home_content from "./Home_content";
+import Searchbar from "./Searchbar";
+import Cards from "../HomeCard/Cards.js";
+import Footer from "../Footer/Footer";
+import { useEffect } from "react";
 
 function Home() {
-const [userInput, setuserInput] = useState("");
-const [userList, setUserList] = useState(list);
-const addHotel=(e)=>{
-    setuserInput(e.target.value);
-    const filterData=list.filter((hotel)=>{
-        if (userInput ==='') {
-            return hotel;
+  const [cityName, setCityName] = useState("Goa");
+  const [hotelData, setHotelData] = useState([]);
+  const [locationId, setlocationId] = useState("");
+  useEffect(() => {
+    searchApi();
+  }, []); //useEffect og getting geoId
+  useEffect(() => {
+    if (locationId) {
+      getHotelData();
+    }
+  }, [locationId]);  //of getting hotel data
+
+
+  const getCityName=(e)=>{
+    // console.log(e.target.value)
+    setCityName(e.target.value);
+    console.log(e.target.name)
+
+  }
+  // console.log(cityName)
+  // console.log(cityName); 
+  //for getting geoId
+  async function searchApi(){
+    const city=cityName;
+    const state=null;
+    const locationID=`${city},India`
+      const options = {
+        method: 'GET',
+        url: 'https://tripadvisor16.p.rapidapi.com/api/v1/hotels/searchLocation',
+        params: {
+        query: {locationID}
+        },
+        headers: {
+          'X-RapidAPI-Key': '3cf2abd49fmshf5f66b40730f54fp1c15bcjsn255695cbedd0',
+          'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
         }
-        else if (hotel.state?.toLowerCase()?.includes(userInput?.toLowerCase())){
-            return hotel;
-        }
-    })
-    setUserList(filterData)
-}
-
- const images =sliderItems.map((url) => (
-
-        <Carousel.Item interval={3000} wrap>
-            <img
-                style={{ height: '100vh' }}
-                className=" d-block w-full"
-                src={url}   
-                alt="First slide"
-            />
-        </Carousel.Item>
-      ));
-    return(
-        <>
-        <Navbar />
-        <div className='h-3/6'>
-        <Carousel controls={false} indicators={false}>{images}</Carousel>
-        </div>
-        <Home_content />
-        <Searchbar
-        userData={userInput}
-         addItem={addHotel}
-         />
-        <div className='card-flex'>  
-
-        {userList.map((card,i)=>(
-            <Cards key={`card-${i}`} id={card.desc} card={card} /> // card-0 card-1
+      };
+      try {
+        const response = await axios.request(options);
+        const geoId=response.data.data[0].geoId;
+        setlocationId(geoId);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    //of getting hotelData
+  async function getHotelData() {
+    const options = {
+      method: "GET",
+      url: "https://tripadvisor16.p.rapidapi.com/api/v1/hotels/searchHotels",
+      params: {
+        geoId: locationId,
+        checkIn: "2023-07-15",
+        checkOut: "2023-07-15",
+        pageNumber: "1",
+        currencyCode: "INR",
+      },
+      headers: {
+        "X-RapidAPI-Key": "3cf2abd49fmshf5f66b40730f54fp1c15bcjsn255695cbedd0",
+        "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com",
+      },
+    };
+    console.log(locationId);
+    try {
+      const response = await axios.request(options);
+      const allData=response.data.data.data;
+      setHotelData(allData);
+    } catch (error) {
+      console.error(error);
+    } 
+  }
+  const images = sliderItems.map((url) => (
+    <Carousel.Item interval={3000} wrap>
+      <img
+        style={{ height: "100vh" }}
+        className=" d-block w-full"
+        src={url}
+        alt="First slide"
+      />
+    </Carousel.Item>
+  ));
+return (
+    <>
+      <Navbar />
+      <div className="h-3/6">
+        <Carousel controls={false} indicators={false}>
+          {images}
+        </Carousel>
+      </div>
+      <Home_content />
+      <Searchbar
+        // userData={userInput}
+         searchCity={getCityName}
+      />
+      <div className="card-flex">
+        {hotelData.map((el, id) => (
+          <Cards key={`card-${id}`} id={id} card={el} /> // card-0 card-1
         ))}
-        </div>
-        <Footer />
-       </>
-    );
+      </div>
+      <Footer />
+    </>
+  );
 }
-  
-  
 export default Home;
--- src/Components/Home/Searchbar.js
+++ src/Components/Home/Searchbar.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React,{useEffect}from "react";
+import axios from "axios";
 import { FiSearch } from "react-icons/fi";
 import { BsFillArrowRightCircleFill } from 'react-icons/bs'
 import "react-datepicker/dist/react-datepicker.css";
 import '../../index.css'
-// import { useState, useRef } from "react";
 
 
-const Searchbar = (props) => {
+const Searchbar =  (props) => {
+  
   
   return (
     <div className="search_bar bg-white absolute  bottom-10 shadow-sm shadow-gray-400">
       <div className="m-auto flex justify-between content-center">
         <div className="search_content">
           <FiSearch className="inline mb-1.5 text-2xl" />
-          <input onChange={props.addItem} type="search" placeholder="Search your fav places" className="ml-3 text-l "/>
+          <input onChange={(props.searchCity)} type="search" placeholder="City,State,Country" className="ml-3 text-l "/>
         </div>
         {/* <div className="date_picker">
           <input type="date" onChange={handleChange} ref={dateInputRef} className='mt-1'/>
